f7b3d993117cd4174c63709d458295dcbcfa9a39
David Janes <david@consensas.com>
2020-10-03 07:36:29 -0400

    order checkpoints

---
+++ test/checkpoints.js
@@ -0,0 +1,59 @@
+/**
+ *  test/checkpoints.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2020-10-02
+ *
+ *  Copyright (2013-2020) David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+const fs = require("iotdb-fs")
+
+const assert = require("assert")
+const path = require("path")
+
+const nlp = require("..")
+const _util = require("./_util")
+
+describe("checkpoints", function() {
+    const _test = _.promise((self, done) => {
+        _.promise(self)
+            .add("path", path.join(__dirname, "data", self.test))
+            .then(fs.read.utf8)
+            .then(nlp.checkpoints)
+            .make(sd => {
+                console.log(sd.tokens)
+            })
+            .end(done, {})
+    })
+
+    it("works (plain text)", function(done) {
+        _.promise({
+            document_media_type: "text/plain",
+            tests: [
+                "study-in-scarlet.txt",
+            ],
+        })
+            .each({
+                method: _test,
+                inputs: "tests:test",
+            })
+            .end(done, {})
+    })
+})
