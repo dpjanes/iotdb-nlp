2c47caab4ff9595e7598acb084fcf17245ea35b7
David Janes <david@consensas.com>
2020-10-05 14:33:05 -0400

    tokenize names and entities (in batch)

---
+++ test/tokenize.entities.js
@@ -0,0 +1,76 @@
+/**
+ *  test/tokenize.entities.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2020-10-03
+ *
+ *  Copyright (2013-2020) David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+const fs = require("iotdb-fs")
+const aws = require("iotdb-awslib")
+
+const assert = require("assert")
+const path = require("path")
+
+const nlp = require("..")
+const _util = require("./_util")
+
+describe("tokenize.entities", function() {
+    const _test = _.promise((self, done) => {
+        _.promise(self)
+            .add("path", path.join(__dirname, "data", self.test))
+            .then(fs.read.utf8)
+            .then(nlp.tokenize.entities)
+            .make(sd => {
+                console.log(JSON.stringify(sd.tokens, null, 2))
+                // console.log(sd.tokens.filter(token => token.tag === "PERSON").forEach(t => console.log(t.document)))
+            })
+            .end(done, {})
+    })
+
+    it("works (plain text)", function(done) {
+        this.timeout(20 * 1000)
+
+        _.promise({
+            verbose: true,
+
+            aws$cfg: require("../.cfg/aws.json").aws$cfg,
+            cache$cfg: {
+                path: path.join(__dirname, "..", ".fs-cache"),
+            },
+
+            document_media_type: "text/plain",
+            tests: [
+                // "bbc_congo.txt",
+                // "harry.txt",
+                // "sherlock.txt",
+                "study-in-scarlet.txt",
+            ],
+        })
+            .then(aws.initialize)
+            .then(aws.comprehend.initialize)
+            .then(fs.cache)
+            .each({
+                method: _test,
+                inputs: "tests:test",
+            })
+            .end(done, {})
+    })
+})
