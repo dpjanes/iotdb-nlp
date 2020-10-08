9578d6031fab5a39f6902efbed2d4e1137ccca8f
David Janes <david@consensas.com>
2020-10-08 12:35:48 -0400

    extraction functions

---
+++ test/extract.persons.js
@@ -0,0 +1,72 @@
+/**
+ *  test/extract.persons.js
+ *
+ *  David Janes
+ *  IOTDB
+ *  2020-10-08
+ *
+ *  Copyright (2013-2020) David P. Janes
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+"use strict"
+
+const _ = require("iotdb-helpers")
+const fs = require("iotdb-fs")
+const aws = require("iotdb-awslib")
+
+const assert = require("assert")
+const path = require("path")
+
+const nlp = require("..")
+const _util = require("./_util")
+
+const WRITE = process.env.WRITE === "1"
+
+describe("extract.persons", function() {
+    const _test = _.promise((self, done) => {
+        const FOLDER = "extract.persons"
+
+        _.promise(self)
+            .then(_util.read_yaml("tokenize.entities", self.filename, "tokens"))
+            .then(nlp.extract.persons)
+            .conditional(WRITE, _util.write_yaml(FOLDER, self.filename, "persons"))
+            .then(_util.read_yaml(FOLDER, self.filename, "want_persons"))
+
+            .make(sd => {
+                const got = sd.persons
+                const want = sd.want_persons
+                assert.deepEqual(got, want)
+            })
+            .end(done, {})
+    })
+
+    it("works (plain text)", function(done) {
+        this.timeout(20 * 1000)
+
+        _.promise({
+            verbose: true,
+
+            document_media_type: "text/plain",
+            tests: [
+                "study-in-scarlet",
+            ],
+        })
+            .each({
+                method: _test,
+                inputs: "tests:filename",
+            })
+            .end(done, {})
+    })
+})
