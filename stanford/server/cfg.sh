f3bb49b8a4db8b6ab01c9eea887d31fe6fd3faef
David Janes <davidjanes@davidjanes.com>
2020-10-17 11:00:58 +0000

    better home for contrib

--- stanford/server/cfg.sh
+++ stanford/server/cfg.sh
@@ -7,17 +7,17 @@
 #
 #   CFG_CP is the classpath to use
 
-CFG_ROOT=$(readlink -f $(dirname $0)/../..)
+CFG_CONTRIB=$(readlink -f $(dirname $0)/../../stanford/contrib)
 CFG_CP=.
 
 PKG_NAME="stanford-ner"
 PKG_VERSION="4.0.0"
-CFG_CP="${CFG_CP}:${CFG_ROOT}/contrib/${PKG_NAME}-${PKG_VERSION}/stanford-ner.jar"
+CFG_CP="${CFG_CP}:${CFG_CONTRIB}/${PKG_NAME}-${PKG_VERSION}/stanford-ner.jar"
 
 PKG_NAME="stanford-tagger"
 PKG_VERSION="4.1.0"
-CFG_CP="${CFG_CP}:${CFG_ROOT}/contrib/${PKG_NAME}-${PKG_VERSION}/stanford-postagger.jar"
+CFG_CP="${CFG_CP}:${CFG_CONTRIB}/${PKG_NAME}-${PKG_VERSION}/stanford-postagger.jar"
 
 PKG_NAME="json-simple"
 PKG_VERSION="1.1.1"
-CFG_CP="${CFG_CP}:${CFG_ROOT}/contrib/${PKG_NAME}-${PKG_VERSION}.jar"
+CFG_CP="${CFG_CP}:${CFG_CONTRIB}/${PKG_NAME}-${PKG_VERSION}.jar"
